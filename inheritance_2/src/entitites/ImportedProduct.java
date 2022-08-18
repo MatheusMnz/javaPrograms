e8809fb2c78b32e225293274dba5c4ad473965d5
Matheus Menezes <matheus.menezes0806@gmail.com>
2022-08-18 14:56:14 -0300

    Inheritance

---
+++ inheritance_2/src/entitites/ImportedProduct.java
@@ -0,0 +1,34 @@
+package entitites;
+
+public class ImportedProduct extends Product {
+	private Double customsFee;
+	
+
+	public ImportedProduct(String name, Double price, Double customsFee) {
+		super(name, price);
+		this.customsFee = customsFee;
+	}
+
+	
+	public Double getCustomsFree() {
+		return customsFee;
+	}
+
+	public void setCustomsFree(Double customsFee) {
+		this.customsFee = customsFee;
+	}
+
+	public Double totalPrice() {
+		return super.getPrice() + customsFee;
+	}
+	
+	@Override
+	public String priceTag() {
+		return getName() 
+				+ " $ " 
+				+ String.format("%.2f", totalPrice())
+				+ " (Customs fee: $ " 
+				+ String.format("%.2f", customsFee)
+				+ ")";
+	}
+}
