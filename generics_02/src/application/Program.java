de3b3d85b7c74f67dc79ac67ef9740f4b6c6c7c3
Matheus Menezes <matheus.menezes0806@gmail.com>
2022-12-22 00:19:50 -0300

    Generics_02 -> Genéricos Delimitados

---
+++ generics_02/src/application/Program.java
@@ -0,0 +1,41 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+import services.CalculationService;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+		
+		String path = "C:\\temp\\in.txt";
+		List<Product> list = new ArrayList<>();
+		
+		try(BufferedReader br = new BufferedReader(new FileReader(path))){
+			String line = br.readLine();
+			while(line != null) {
+				String[] fields = line.split(",");
+				list.add(new Product(fields[0], Double.parseDouble(fields[1])));
+				line = br.readLine();
+			}
+			Product x = CalculationService.max(list);
+			System.out.println("Most Expensive: ");
+			System.out.println(x);
+		}
+		catch(IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		
+		sc.close();
+	}
+
+}
